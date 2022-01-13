41ae9d106a03af12504498006b66ea287160d4d6
Mousam Premi <Mousam.Premi@wheelsemi.com>
2022-01-13 13:03:52 +0530

    modified port number

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
 const port = process.env.PORT || 8000;
 
-console.log("Hello world");
+app.listen(port, () =>{
+    console.log('listening to the port at' ,{port})
+})
