2a1cd10b19bcdd0af5ea5472e0d89594066ec43d
Mousam Premi <Mousam.Premi@wheelsemi.com>
2022-01-13 13:18:01 +0530

    added console log

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8000;
 
+console.log("Deployed first app in heroku:Mousam")
+
 app.listen(port, () =>{
     console.log('listening to the port at' ,{port})
 })
