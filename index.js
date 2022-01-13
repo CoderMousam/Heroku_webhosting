5d582a17fdd68fe3c39f204080170ed89c55802b
Mousam Premi <Mousam.Premi@wheelsemi.com>
2022-01-13 13:58:27 +0530

    modified app get

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
+const { status } = require('express/lib/response');
 const app = express();
 const port = process.env.PORT || 8000;
 
@@ -9,3 +10,6 @@ console.log("Deployed first app in heroku:Mousam")
 app.listen(port, () =>{
     console.log('listening to the port at' ,{port})
 })
+app.get('/', function (req, res) {
+    res.status(status).send('index', {});
+  });
